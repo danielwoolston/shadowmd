ae8fe51fa5199859de300b0ea38efa79dc027503
danielwoolston <danwoolston@gmail.com>
2024-05-30 10:17:15 -0400

    folder creation

--- Shadow_MD_DualController_Template/Shadow_MD_DualController_Template.ino
+++ Shadow_MD_DualController_Template/Shadow_MD_DualController_Template.ino
@@ -1,26 +1,10 @@
 // =======================================================================================
 //        SHADOW_MD:  Small Handheld Arduino Droid Operating Wand + MarcDuino
 // =======================================================================================
-//                          Last Revised Date: 08/23/2015
-//                             Revised By: vint43
+//                          Last Revised Date: 05/30/2024
+//                             Revised By: danielwoolston
 //                Inspired by the PADAWAN / KnightShade SHADOW effort
 // =======================================================================================
-//
-//         This program is free software: you can redistribute it and/or modify it for
-//         your personal use and the personal use of other astromech club members.  
-//
-//         This program is distributed in the hope that it will be useful 
-//         as a courtesy to fellow astromech club members wanting to develop
-//         their own droid control system.
-//
-//         IT IS OFFERED WITHOUT ANY WARRANTY; without even the implied warranty of
-//         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//
-//         You are using this software at your own risk and, as a fellow club member, it is
-//         expected you will have the proper experience / background to handle and manage that 
-//         risk appropriately.  It is completely up to you to insure the safe operation of
-//         your droid and to validate and test all aspects of your droid control system.
-//
 // =======================================================================================
 //   Note: You will need a Arduino Mega ADK rev3 to run this sketch,
 //   as a normal Arduino (Uno, Duemilanove etc.) doesn't have enough SRAM and FLASH
